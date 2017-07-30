819d627d45b5a4f3ae4646dc4cc1ff8875017b96
erhmeh <jaminearly@gmail.com>
2017-07-30 19:54:30 +1000

    Add files via upload

---
+++ 02-12_value1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+	printf("Here is a value: %d\n",i);
+	return(0);
+}
+
---
+++ 03-03_whileendlessbreak.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+
+	while(1)
+	{
+		puts("Here I go...!");
+		a++;
+		if( a > 10)
+			break;
+	}
+
+	return(0);
+}
